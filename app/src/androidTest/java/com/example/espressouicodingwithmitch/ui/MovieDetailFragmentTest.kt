3ec3ab4e97def6e368a8d21da898d814d1e22285
ernest75 <ernestschneider@gmail.com>
2021-07-06 12:15:17 +0200

    Added tests for fragments

---
+++ app/src/androidTest/java/com/example/espressouicodingwithmitch/ui/MovieDetailFragmentTest.kt
@@ -0,0 +1,86 @@
+package com.example.espressouicodingwithmitch.ui
+
+import org.junit.Assert.*
+import android.app.Activity
+import android.app.Instrumentation.*
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.bumptech.glide.request.RequestOptions
+import com.example.espressouicodingwithmitch.R
+import com.example.espressouicodingwithmitch.ui.ImageViewHasDrawableMatcher.hasDrawable
+import com.example.espressouicodingwithmitch.ui.data.Movie
+import com.example.espressouicodingwithmitch.ui.data.source.MoviesDataSource
+import com.example.espressouicodingwithmitch.ui.factory.MovieFragmentFactory
+import io.mockk.every
+import io.mockk.mockk
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MovieDetailFragmentTest{
+
+
+    @Test
+    fun isMovieDataVisible() {
+        // SETUP
+        val movieId = 1
+        val title = "The Rundown"
+        val description =
+            "A tough aspiring chef is hired to bring home a mobster's son from the Amazon but " +
+                    "becomes involved in the fight against an oppressive town operator and the search " +
+                    "for a legendary treasure."
+        val movie = Movie(
+            movieId,
+            title,
+            "https://nyc3.digitaloceanspaces.com/open-api-spaces/open-api-static/blog/1/The_Rundown-the_rundown.png",
+            description,
+            arrayListOf("R.J. Stewart", "James Vanderbilt"),
+            arrayListOf(
+                "Dwayne Johnson",
+                "Seann William Scott",
+                "Rosario Dawson",
+                "Christopher Walken"
+            )
+        )
+
+        val movieDataSource = mockk<MoviesDataSource>()
+        every {
+            movieDataSource.getMovie(any())
+        } returns movie
+
+        val requestOptions = RequestOptions()
+            .placeholder(R.drawable.default_image)
+            .error(R.drawable.default_image)
+
+        val fragmentFactory = MovieFragmentFactory(requestOptions,movieDataSource)
+        val bundle = Bundle()
+        bundle.putInt("movie_id",movieId)
+        val scenario = launchFragmentInContainer<MovieDetailFragment>(
+            fragmentArgs = bundle,
+            factory = fragmentFactory
+        )
+
+        //Verify
+        onView(withId(R.id.movie_title)).check(matches(withText(title)))
+        onView(withId(R.id.movie_description)).check(matches(withText(description)))
+
+    }
+}
